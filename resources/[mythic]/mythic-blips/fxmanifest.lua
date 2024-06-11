0a96c51fd5dcdd43575d45c817f1e2fff94a5b53
Greve <frankgrevekrogh@gmail.com>
2024-06-11 21:33:11 +0200

    files

---
+++ resources/[mythic]/mythic-blips/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+client_script "@mythic-base/components/cl_error.lua"
+client_script "@mythic-pwnzor/client/check.lua"
+
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    'client/*.lua',
+}
